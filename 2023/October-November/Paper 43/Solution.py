256717a3c986e6f2fdd4dbf3af864bda67b9f333
Md Shourov Hasan <shourov5275@gmail.com>
2025-10-24 18:16:14 +0600

    Fixed pointer manupulations in Queue and finished solving question 2

--- 2023/October-November/Paper 43/Solution.py
+++ 2023/October-November/Paper 43/Solution.py
@@ -38,24 +38,24 @@ Queue = [] #stores 50 strings
 HeadPointer = -1
 TailPointer = 0
 
-#need to fix some issues here
 def Enqueue(DataToAdd):
     global TailPointer, HeadPointer
-    HeadPointer = 0
 
     if TailPointer > 49:
         print("Queue is full!")
     else:
+        if HeadPointer == -1:
+            HeadPointer = 0
         Queue.append(DataToAdd)
         TailPointer += 1
 
 def Dequeue():
     global  HeadPointer
     if HeadPointer == -1:
-        print("Empty")
+        return "Empty"
     else:
         DataToRemove = Queue[HeadPointer]
-        HeadPointer -= 1
+        HeadPointer += 1
         return DataToRemove     
 
 def ReadData():
@@ -63,10 +63,53 @@ def ReadData():
     for line in file:
         Enqueue(line.strip())
     file.close()
-    
 
 
-#Question 3
+class RecordData:
+    def __init__(self, ID,Total):
+        self.ID = ID #String
+        self.Total = Total #Integer
+
+global Records, NumberRecords
+
+Records = [] #Stores 50 items of type RecordData
+NumberRecords = 0 #Integer
+
+def TotalData():
+    global Records, NumberRecords
+    DataAccessed = Dequeue()
+    Flag = False
+    if NumberRecords == 0:
+        Records.append(RecordData(DataAccessed,1))
+        Flag = True
+        NumberRecords += 1
+    else:
+        for X in range(0, NumberRecords):
+            if Records[X].ID == DataAccessed:
+                Records[X].Total += 1
+                Flag = True
+
+    if Flag == False:
+        Records.append(RecordData(DataAccessed,1))
+        NumberRecords += 1
+
+def OutputRecords():
+    for _ in range(0,len(Records)):
+        print(f"ID {Records[i].ID} Total {Records[i].Total}")
+
+
+#main program
+ReadData()
+
+for i in range(0,len(Queue)):
+    TotalData()
+
+OutputRecords()
+
+
+
+
+Question 3
 
 class Character:
     def __init__(self, Name, XPosition, YPosition):
