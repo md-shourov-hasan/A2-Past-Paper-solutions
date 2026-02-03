38d21501596758e8908a71bdcaa321e631c20274
MD SHOUROV HASAN <shourov5275@gmail.com>
2026-02-03 18:57:15 +0600

    Solved question 2 and partially solved question 3
    Encountered a problem in question 3, which made me question the logic of the problem.

---
+++ 2022/May - June/Paper 42/Question3_J22.py
@@ -0,0 +1,41 @@
+class Card:
+  def __init__(self, Number, Colour):
+    self.__Number = Number # Integer
+    self.__Colour = Colour # String
+
+  def GetNumber(self):
+    return self.__Number
+
+  def GetColour(self):
+    return self.__Colour
+
+
+def ChooseCard():
+  index = int(input("Enter an index: "))
+
+  while index >= 1 and index <= 30:
+    
+
+
+
+
+
+
+
+#main program
+
+Cards = [] #Stores 30 elements of type Card
+
+
+CardFile = open("CardValues.txt","r")
+
+for line in CardFile:
+  number = int(line.strip())
+  Colour = CardFile.readline().strip()
+
+  Cards.append(Card(number, Colour))
+
+CardFile.close()
+
+ChosenCards = []
+
