6cbc1dd66388a809dbdb69d8a6da7f56cbd1b209
MD SHOUROV HASAN <shourov5275@gmail.com>
2026-04-30 19:56:18 +0600

    Paper 41 and Paper 43 were the SAMEEEE!!

---
+++ 2023/October-November/Paper 43/Question1_N23.py
@@ -0,0 +1,30 @@
+# Question 1
+
+
+def IterativeVowels(Value):
+    Total = 0
+    LengthString = len(Value)
+    for x in range(LengthString):
+        FirstCharacter = Value[0:1]
+        if FirstCharacter == 'a' or FirstCharacter == 'e' or FirstCharacter == 'i' or FirstCharacter == 'o' or FirstCharacter == 'u':
+            Total += 1
+        Value = Value[1:len(Value)]
+
+    return Total
+
+
+print(IterativeVowels("house"))
+
+
+def RecursiveVowels(Value):
+    if len(Value) == 0:
+        return 0
+    FirstCharacter = Value[0:1]
+    if FirstCharacter == 'a' or FirstCharacter == 'e' or FirstCharacter == 'i' or FirstCharacter == 'o' or FirstCharacter == 'u':
+        Value = Value[1:len(Value)]
+        return 1 + RecursiveVowels(Value)
+    else:
+        return RecursiveVowels(Value[1:len(Value)])
+
+
+print(RecursiveVowels("imagine"))
